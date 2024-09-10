1dee0f4c5eb6d004a2ebdd7af5a0cc0d8b3b5f1e
bhatsrinidhi <bhatsrinidhi95@gmail.com>
2024-09-09 22:44:16 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById("addRecordButton").addEventListener("click", addRecord);
+
+function addRecord() {
+    //TODO: make validation to input field
+    //get value from inputs
+    var teamName = document.getElementById("teamID").value;
+    var description = document.getElementById("description").value;
+
+    console.log(teamName);
+    console.log(description);
+
+    var recordDiv = document.createElement("div");
+
+    // create new record container
+    var teamIDDiv = document.createElement("div");
+    var descriptionDiv = document.createElement("div");
+    var statusDiv = document.createElement("div");
+
+    recordDiv.className="row"
+    teamIDDiv.textContent = teamName
+    teamIDDiv.className="cell"
+    descriptionDiv.textContent = description
+    descriptionDiv.className="cell"
+    statusDiv.textContent = "Created"
+    statusDiv.className = "cell"
+
+
+
+    recordDiv.appendChild(teamIDDiv);
+    recordDiv.appendChild(descriptionDiv);
+    recordDiv.appendChild(statusDiv);
+
+    document.getElementById("recordsContainer").appendChild(recordDiv);
+}
